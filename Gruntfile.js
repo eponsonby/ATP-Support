ddebc65301bb34edeefc3b755648bf702886b0fb
Matthew Dingee <matthew.dingee@opm.gov>
2017-11-16 12:05:43 -0500

    Turns out lastBuildDate is invalid because it's from RSS. Removed.

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,8 +50,7 @@ module.exports = function(grunt) {
             'Attribute “⚡” not allowed on element “html” at this point.',
             'Element “img” is missing required attribute “src”.',
             'Attribute “amp-custom” not allowed on element “style” at this point.',
-            'Attribute “amp-boilerplate” not allowed on element “style” at this point.',
-            'Element “lastbuilddate” not allowed as child of element “p” in this context.'
+            'Attribute “amp-boilerplate” not allowed on element “style” at this point.'
           ]
         },
         src: [ '_dist/**/*.html' ]
